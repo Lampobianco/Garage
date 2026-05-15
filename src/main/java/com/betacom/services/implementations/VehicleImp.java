b00203913fb2cc772145f5bee459021e0c867538
Lampobianco <daniele.lometti@gmail.com>
2026-05-15 12:14:19 +0200

    Aggiunta l'interfaccia VehicleImp

---
+++ src/main/java/com/betacom/services/implementations/VehicleImp.java
@@ -0,0 +1,10 @@
+package com.betacom.services.implementations;
+
+public interface VehicleImp {
+
+    boolean wheelCountCheck();
+    boolean fuelTypeCheck();
+    boolean categoryCheck();
+    boolean productionYearCheck();
+
+}
