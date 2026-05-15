509a49b05b8f0a461164fab51c866d3eede25408
Stefan Purcea <purceastefan06@gmail.com>
2026-05-15 11:41:37 +0200

    creazione SingleTon

--- src/main/java/com/betacom/singleton/SingleTon.java
+++ src/main/java/com/betacom/singleton/SingleTon.java
@@ -1,4 +1,36 @@
 package com.betacom.singleton;
 
-public class SingleTon {
+import com.betacom.models.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleTon {    //per gestire l'accesso alla lista
+
+    private static SingleTon istance=null;
+    private static List <Vehicle> veicoli = null;
+
+    private SingleTon() {
+
+
+
+    }
+
+    private SingleTon getInstance() {   //creazione singleton se non esiste
+        if(istance == null) {
+            istance=new SingleTon();
+        }
+
+        return istance;
+    }
+
+    public List <Vehicle> getList() { //crea lista se non esiste, la sostituisce sempre
+        if(getInstance().veicoli == null) {
+            veicoli = new ArrayList <Vehicle> ();
+        }
+
+        return veicoli;
+    }   //per operare sulla lista bisogna chiamare SingleTon.getList() e aggiungere metodi tipo .add() ecc
+
+
 }
