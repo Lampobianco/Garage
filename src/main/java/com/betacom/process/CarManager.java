5a5d74e95920feaaabf2bce26831257b7608719e
Lampobianco <daniele.lometti@gmail.com>
2026-05-15 16:10:27 +0200

    Primi Metodi implementati in CarManager e CarImp con stampe

--- src/main/java/com/betacom/process/CarManager.java
+++ src/main/java/com/betacom/process/CarManager.java
@@ -1,10 +1,26 @@
 package com.betacom.process;
 
+import com.betacom.models.Car;
+import com.betacom.models.Vehicle;
 import com.betacom.services.Interfaces;
+import com.betacom.services.implementations.CarImp;
 
 public class CarManager implements Interfaces {
     @Override
     public boolean execute() throws Exception {
+
+
+        Vehicle car = new Car();
+        car.setWheelCount(4);
+        car.setFuelType("benzina");
+        car.setCategory("fuoristrada");
+        car.setProductionYear(2018);
+
+        CarImp ci1 = new CarImp();
+        ci1.wheelCountCheck(car);
+        ci1.fuelTypeCheck(car);
+        ci1.categoryCheck(car);
+        ci1.productionYearCheck(car);
 
 
         return false;
--- src/main/java/com/betacom/services/implementations/CarImp.java
+++ src/main/java/com/betacom/services/implementations/CarImp.java
@@ -1,9 +1,82 @@
 package com.betacom.services.implementations;
 
-public class CarImp {
+import com.betacom.exceptions.AcademyException;
+import com.betacom.models.Car;
+import com.betacom.models.Vehicle;
+
+public class CarImp implements VehicleImp{
 
 
+    @Override
+    public void wheelCountCheck(Vehicle v) throws AcademyException{
+        if (v instanceof Car) {
+
+            if (v.getWheelCount() != 4) {
+                throw new AcademyException("Errore: Un veicolo di tipo Car deve avere esattamente 4 ruote!");
+            }else{
+
+                System.out.println("Numero di ruote corretto !");
+
+            }
+        }
+
+    }
+
+    @Override
+    public void fuelTypeCheck(Vehicle v) throws AcademyException{
+
+        if (v instanceof Car) {
+            String fuel = v.getFuelType();
+
+            // Se NON è benzina AND NON è elettrico AND...
+            if (!(fuel.equals("benzina") || fuel.equals("elettrico") ||
+                    fuel.equals("ibrida") || fuel.equals("diesel"))) {
+
+                throw new AcademyException("Tipo alimentazione NON consentito: " + fuel);
+            }else{
+
+                System.out.println("Tipo alimentazione corretto !");
+
+            }
+        }
+
+    }
+
+    @Override
+    public void categoryCheck(Vehicle v) throws AcademyException{
+
+        if (v instanceof Car) {
+            String cat = v.getCategory();
+
+            if (cat == null || cat.isEmpty()) {
+                throw new AcademyException("Errore: La categoria della macchina è mancante!");
+            }
+
+            String lowCat = cat.toLowerCase();
+
+            if (!(lowCat.equals("suv") || lowCat.equals("fuoristrada") || lowCat.equals("utilitaria")
+                    || lowCat.equals("berlina") || lowCat.equals("station wagon") || lowCat.equals("sportiva"))) {
+
+                throw new AcademyException("Categoria '" + cat + "' non riconosciuta per il tipo Car.");
+            }else{
+
+                System.out.println("Tipo alimentazione corretto !");
+
+            }
+        }
 
 
+    }
 
+    @Override
+    public void productionYearCheck(Vehicle v) throws AcademyException {
+
+        int y = v.getProductionYear();
+
+        if (y < 2006) {
+
+            throw new AcademyException("Errore: L'anno di produzione (" + y + ") non può essere precedente al 2006!");
+        }
+        System.out.println("Anno di produzione corretto");
+    }
 }
